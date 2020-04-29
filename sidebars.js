7b5529ca3471ba862ece3696a71df620b33ab50b
Mohamed Yussuf <yussuf@revenuemonster.my>
2020-04-29 20:34:36 +0800

    chore: change name

--- sidebars.js
+++ sidebars.js
@@ -4,7 +4,7 @@ module.exports = {
       "introduction/overview",
       "introduction/usecase",
     ],
-    QuickStart: [
+    "Quick Start": [
       "quickstart/sdk",
       "quickstart/developer-application",
     ],
