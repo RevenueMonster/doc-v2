af932af48ba4566818eb320f709ae5856d1400b3
EdwinLeong <zhiyi@revenuemonster.my>
2020-05-28 22:58:55 +0800

    feat: add payment page ( quick-pay , transaction QR)

--- sidebars.js
+++ sidebars.js
@@ -1,12 +1,20 @@
 module.exports = {
   someSidebar: {
-    Introduction: [
-      "introduction/overview",
-      "introduction/usecase",
-    ],
-    "Quick Start": [
-      "quickstart/sdk",
-      "quickstart/developer-application",
+    Introduction: ["introduction/overview", "introduction/usecase"],
+    "Quick Start": ["quickstart/sdk", "quickstart/developer-application"],
+    Payment: [
+      "payment/rm-payment",
+
+      {
+        type: "category",
+        label: "Quick Pay",
+        items: [
+          "payment/quickpay/quick-pay",
+          "payment/quickpay/refund",
+          "payment/quickpay/reverse",
+        ],
+      },
+      "payment/transactionQR",
     ],
   },
 };
