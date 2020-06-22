e99d6e07c0c2eaa706f773d4d4c4b19024df7c55
EdwinLeong <zhiyi@revenuemonster.my>
2020-06-22 22:01:15 +0800

    fix: UI design

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import classnames from 'classnames';
-import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import useBaseUrl from '@docusaurus/useBaseUrl';
-import styles from './styles.module.css';
+import React from "react";
+import classnames from "classnames";
+import Layout from "@theme/Layout";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import useBaseUrl from "@docusaurus/useBaseUrl";
+import styles from "./styles.module.css";
 
 const features = [
   {
     title: <>Unified Payment</>,
-    imageUrl: 'img/unified-payment.png',
+    imageUrl: "img/unified-payment.png",
     description: (
       <>
         Empower your business with easy access to all mobile payment channels.
@@ -18,28 +18,30 @@ const features = [
   },
   {
     title: <>Loyalty 2.0 (Social Media)</>,
-    imageUrl: 'img/loyalty.png',
+    imageUrl: "img/loyalty.png",
     description: (
       <>
-        One platform for all your loyalty program and social media needs. Wechat Official Account and Facebook Messenger ready.
+        One platform for all your loyalty program and social media needs. Wechat
+        Official Account and Facebook Messenger ready.
       </>
     ),
   },
   {
     title: <>Financial Cloud</>,
-    imageUrl: 'img/financial-cloud.png',
+    imageUrl: "img/financial-cloud.png",
     description: (
       <>
-        Secure and accelerate your Fintech innovation with our expertise in Alibaba Cloud.
+        Secure and accelerate your Fintech innovation with our expertise in
+        Alibaba Cloud.
       </>
     ),
   },
 ];
 
-function Feature({imageUrl, title, description}) {
+function Feature({ imageUrl, title, description }) {
   const imgUrl = useBaseUrl(imageUrl);
   return (
-    <div className={classnames('col col--4', styles.feature)}>
+    <div className={classnames("col col--4", styles.feature)}>
       {imgUrl && (
         <div className="text--center">
           <img className={styles.featureImage} src={imgUrl} alt={title} />
@@ -53,22 +55,24 @@ function Feature({imageUrl, title, description}) {
 
 function Home() {
   const context = useDocusaurusContext();
-  const {siteConfig = {}} = context;
+  const { siteConfig = {} } = context;
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <header className={classnames('hero hero--primary', styles.heroBanner)}>
+      description="Description will go into a meta tag in <head />"
+    >
+      <header className={classnames("hero hero--primary", styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
           <p className="hero__subtitle">{siteConfig.tagline}</p>
           <div className={styles.buttons}>
             <Link
               className={classnames(
-                'button button--outline button--secondary button--lg',
-                styles.getStarted,
+                "button button--outline button--secondary button--lg",
+                styles.getStarted
               )}
-              to={useBaseUrl('docs/introduction/overview')}>
+              to={useBaseUrl("docs/introduction/overview")}
+            >
               Get Started
             </Link>
           </div>
