4403ddf82aba4b99bdbcdfd5bc3749f327ba673e
chingcong <w.chingcong@gmail.com>
2021-07-21 15:37:01 +0800

    feat: ekyc documentation

--- sidebars.js
+++ sidebars.js
@@ -226,5 +226,6 @@ module.exports = {
     // FAQ: ["introduction/overview"],
     Appendix: ["product-terms", "error-codes"],
     "Plug-in": ["plug-in/wooCommerce", "plug-in/opencart"],
+    "EKYC": ["ekyc/mykad-recognition", "ekyc/face-verification"],
   },
 };
