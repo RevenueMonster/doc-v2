aff7d16f414d4639d021418423d77ea5c0d469e5
Oskang09 <developer.oskang@gmail.com>
2023-11-20 12:19:04 +0800

    fix: meta

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,9 +8,42 @@ module.exports = {
   favicon: "img/favicon.ico",
   organizationName: "revenuemonster", // Usually your GitHub org/user name.
   projectName: "doc-v2", // Usually your repo name.
-
   plugins: [path.resolve(__dirname, "./node_modules/docusaurus-lunr-search/")],
   themeConfig: {
+    metadata: [
+      {
+        name: "keywords", content: [
+          "revenue monster",
+          "apac venture",
+          "alacarte",
+          "terminal payment",
+          "payment link",
+          "payment",
+          "unified payment",
+          "wallet payment",
+          "malaysia payment gateway",
+          "payment gateway",
+          "fintech",
+          "digitalization",
+          "payment solution",
+          "documentation",
+          "payment integration",
+          "wallet integration",
+        ].join(",")
+      },
+      { name: "author", content: "Revenue Monster" },
+      { name: "description", content: "Revenue Monster pioneers fintech solutions to help fast-track business digitalization. We empower businesses with a fully digital ecosystem that provides a full suite of services critical to business digitalisation. We provide solutions from digital payment solutions, e-loyalty & rewards, online store, to social media management." },
+      { name: "og:site_name", content: "Revenue Monster" },
+      { name: "og:type", content: "website" },
+      { name: "og:url", content: "https://doc.revenuemonster.my" },
+      { name: "og:title", content: "Documentation | Revenue Monster | Experience True Unified Payment" },
+      { name: "og:description", content: "Revenue Monster pioneers fintech solutions to help fast-track business digitalization. We empower businesses with a fully digital ecosystem that provides a full suite of services critical to business digitalisation. We provide solutions from digital payment solutions, e-loyalty & rewards, online store, to social media management." },
+      { name: "og:image", content: "https://directus.superapp.my/assets/4bec8b80-eaaa-4a71-b2e6-3dd26c7afede.png" },
+      { name: "twitter:card", content: "summary_large_image" },
+      { name: "twitter:url", content: "https://doc.revenuemonster.my" },
+      { name: "twitter:title", content: "Documentation | Revenue Monster | Experience True Unified Payment" },
+      { name: "twitter:image", content: "https://directus.superapp.my/assets/4bec8b80-eaaa-4a71-b2e6-3dd26c7afede.png" },
+    ],
     colorMode: {
       defaultMode: "light",
       disableSwitch: true,
